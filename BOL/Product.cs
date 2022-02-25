455b05d7d8071939b6877cfef12b378ecaa9beee
sumit27b <87543002+sumit27b@users.noreply.github.com>
2022-02-25 14:08:18 +0530

    Add project files.

---
+++ BOL/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public string Category { get; set; }
+        public int Likes { get; set; }
+    }
+}
---
+++ DAL/EmployeeDao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using BOL;
+
+namespace DAL
+{
+    public static class EmployeeDao
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+                              //using system.configuration           //same name as in App.config from ecommerceTestApp 
+            string conString = ConfigurationManager.ConnectionStrings["mysqlactsdb"].ConnectionString;
+
+            MySqlConnection con =new MySqlConnection(conString);
+
+            try
+            {
+                MySqlCommand cmd =new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "select * from employees";
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return employees;
+        }
+    }
+}
